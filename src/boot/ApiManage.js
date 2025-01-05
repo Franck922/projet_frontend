6d83c966cf763cf2f24a88e80fa6e19bd61f81eb
mouafo <hari.randoll@gmail.com>
2025-01-05 13:17:50 +0100

    ok

---
+++ src/boot/ApiManage.js
@@ -0,0 +1,4 @@
+export class ApiManage {
+  baseUrl = "https://api.horizon-security.cm";
+  // baseUrl = "http://127.0.0.1:8000";
+}
---
+++ src/menuAside.js
@@ -0,0 +1,41 @@
+import {
+  mdiAccountCircle,
+  mdiMonitor,
+  mdiLock,
+  mdiAlertCircle,
+  mdiSquareEditOutline,
+  mdiTable,
+  mdiViewList,
+  mdiTelevisionGuide,
+  mdiResponsive,
+  mdiPalette,
+  mdiAccountSwitch,
+  mdiBell,
+  mdiHomeGroup,
+  mdiCommentProcessing,
+  mdiPhoneSettings,
+} from "@mdi/js";
+
+export default [
+  {
+    to: "/",
+    icon: mdiMonitor,
+    label: "Dashboard",
+  },
+
+  {
+    to: "/Paiement",
+    icon: mdiHomeGroup,
+    label: "Paiement",
+  },
+  {
+    to: "/Poste",
+    label: "Poste Employe",
+    icon: mdiAccountSwitch,
+  },
+  {
+    to: "/Sanction",
+    label: "Sanctions ",
+    icon: mdiAccountSwitch,
+  },
+];
